58df13eb3935767a79afd451fd1d916a53751e00
yoshiyukisato0304 <yoshiyuki.sato0304@gmail.com>
2025-10-28 13:05:19 +0900

    loginをFBVに修正

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,20 +1,28 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import View
 from django.views.generic import TemplateView
-from django.shortcuts import redirect
-from django.contrib.auth import logout
+from django.shortcuts import redirect,render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class UserLoginView(LoginView):
-    template_name = 'login.html'
 
+def loginview(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'ユーザー名またはパスワードが正しくありません。')
+            return redirect('login')
+    return render(request, 'login.html')
 
 def logoutcheck(request):
     logout(request)
     return redirect('login')
 
-class UserLogoutView(LogoutView):
-    pass
 
