dc5c89280bcfefc2e94194e003689e918b7ab7db
yoshiyukisato0304 <yoshiyuki.sato0304@gmail.com>
2025-11-01 11:40:50 +0900

    customuserのroleを別テーブルに分割

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -2,15 +2,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    ROLE_CHOICES = [
-        ('student', '生徒'),
-        ('teacher', '教師'),
-        ('staff', 'スタッフ'),
-    ]
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    role = models.ForeignKey('Role',default=1,on_delete=models.CASCADE)
     xp = models.IntegerField(default=0)
     approved = models.BooleanField(default=False)
     last_reset = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.username
+
+class Role(models.Model):
+    ROLE_CHOICES = [
+        ('student', '生徒'),
+        ('teacher', '教師'),
+        ('staff', 'スタッフ'),
+    ]
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return self.name
